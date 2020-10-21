2bb3d858a9be187fa2ce9dd864c73619b4561a3a
Мадорная Александра Олеговна <mao@udcs.local>
2020-10-21 17:55:37 +0300

    closePopup

--- script.js
+++ script.js
@@ -1,13 +1,12 @@
 let btnEdit = document.querySelector('.profile__edit-button'); // Кнопка редактировать профиль
-let btnClose = document.querySelector('popup__close'); // Крестик (закрыть)
-let btnSave = document.querySelector('popup__save'); // Кнопка "Сохранить"
 
 
-let inputName = document.querySelector('popup__name'); // 
-let inputJob = document.querySelector('popup__job'); // 
 
-let userName = document.querySelector('profile__name'); // 
-let userJob = document.querySelector('profile__job'); //
+let inputName = document.querySelector('.popup__name'); // 
+let inputJob = document.querySelector('.popup__job'); // 
+
+let userName = document.querySelector('.profile__name'); // 
+let userJob = document.querySelector('.profile__job'); //
 
 function openPopup() {
     let popup = document.querySelector('.popup');
@@ -15,3 +14,16 @@ function openPopup() {
 }
 
 btnEdit.addEventListener('click', openPopup);
+
+function closePopup() {
+    let btnClose = document.querySelector('.popup__close'); // Крестик (закрыть)
+    popup.className.toggle('popup');
+}
+
+btnClose.addEventListener('click', closePopup);
+
+function savePopup() {
+    let btnSave = document.querySelector('.popup__save'); // Кнопка "Сохранить"
+
+}
+btnSave.addEventListener('click', savePopup);
