0eb2040d84c4ef314ee7cc5d1219c8bd8f8cfa28
n13cwg <n13cwg@gmail.com>
2020-10-21 22:31:11 +0300

    .

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 let btnEdit = document.querySelector('.profile__edit-button'); // Кнопка редактировать профиль
-
-
+let btnClose = document.querySelector('.popup__close'); // Создала btnClose и присвоила ей значение <button class="popup__close">
+let btnSave = document.querySelector('.popup__save'); // Создала btnSave и присвоила ей значение <button class="popup__save">
 
 let inputName = document.querySelector('.popup__name'); // 
 let inputJob = document.querySelector('.popup__job'); // 
@@ -16,14 +16,13 @@ function openPopup() { // Фунция для открытия popup
 btnEdit.addEventListener('click', openPopup); // Собитие, при нажании на <button class="profile__edit-button"> срабатывает function openPopup()
 
 function closePopup() { // Фунция для закрытия popup
-    let btnClose = document.querySelector('.popup__close'); // Создала btnClose и присвоила ей значение <button class="popup__close">
-    popup.className.toggle('popup'); // Вернула класс .popup для закрытиня попапа
+    let popup = document.querySelector('.popup_opened');
+    popup.className = 'popup'; // Вернула класс .popup для закрытиня попапа
 }
 
 btnClose.addEventListener('click', closePopup); // Собитие, при нажании на <button class="popup__close"> срабатывает function closePopup()
 
 function savePopup() { // Фунция для закрытия popup и заменя содержимого в <h1 class="profile__name"> и <p class="profile__job">
-    let btnSave = document.querySelector('.popup__save'); // Создала btnSave и присвоила ей значение <button class="popup__save">
 
 }
 btnSave.addEventListener('click', savePopup);
