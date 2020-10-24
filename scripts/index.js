3702384e02735e4a38431905c9d9a4925724efb9
n13cwg <n13cwg@gmail.com>
2020-10-25 00:18:54 +0300

    .

--- scripts/index.js
+++ scripts/index.js
@@ -1,16 +1,24 @@
-// Скрипт для открытия попапа редактирования профиля:
+// Скрипт для редактирования профиля:
 let popup = document.querySelector('.popup');
+let popupImg = document.querySelector('.');
 
 let btnEdit = document.querySelector('.profile__edit-button');
 let btnClose = document.querySelector('.popup__close');
+let btnImgAdd = document.querySelector('.');
+let btnImgClose = document.querySelector('.');
 
+let popupImgCjntainer = document.querySelector('.');
 let popupContainer = document.querySelector('.popup__container');
 
 let inputName = document.querySelector('.popup__input_name');
 let inputJob = document.querySelector('.popup__input_job');
+let inputImgName = document.querySelector('.');
+let inputImgAddress = document.querySelector('.');
 
 let userName = document.querySelector('.profile__name');
 let userJob = document.querySelector('.profile__job');
+let imgName = document.querySelector('.');
+let imgAddress = document.querySelector('.');
 
 function openPopup() {
   popup.classList.add('popup_opened');
@@ -33,4 +41,9 @@ btnClose.addEventListener('click', closePopup);
 btnEdit.addEventListener('click', openPopup);
 popupContainer.addEventListener("submit", savePopup);
 
-// Скрипт для открытия попапа добавления карточек, удаление карточек:
+// Скрипт для добавления карточек, удаление карточек:
+// 1. открыть/закрыть попап
+// 2. добавление новых карточек
+// 3. удалить карточку
+// 4. закрасить лайки
+// 5. плавное появление попапов
