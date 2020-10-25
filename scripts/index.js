11550523f4bd8aa985896121b87004334ff3be4c
n13cwg <n13cwg@gmail.com>
2020-10-25 11:37:02 +0300

    .

--- scripts/index.js
+++ scripts/index.js
@@ -1,24 +1,24 @@
 // Скрипт для редактирования профиля:
 let popup = document.querySelector('.popup');
-let popupImg = document.querySelector('.');
+let popupImg = document.querySelector('.popup-img');
 
 let btnEdit = document.querySelector('.profile__edit-button');
 let btnClose = document.querySelector('.popup__close');
-let btnImgAdd = document.querySelector('.');
-let btnImgClose = document.querySelector('.');
+let btnImgAdd = document.querySelector('.profile__button');
+let btnImgClose = document.querySelector('.popup__close-img');
 
-let popupImgCjntainer = document.querySelector('.');
+let popupImgContainer = document.querySelector('.popup__container-img');
 let popupContainer = document.querySelector('.popup__container');
 
 let inputName = document.querySelector('.popup__input_name');
 let inputJob = document.querySelector('.popup__input_job');
-let inputImgName = document.querySelector('.');
-let inputImgAddress = document.querySelector('.');
+let inputImgName = document.querySelector('.popup__input_name-img');
+let inputImgAddress = document.querySelector('.popup__input_address');
 
 let userName = document.querySelector('.profile__name');
 let userJob = document.querySelector('.profile__job');
-let imgName = document.querySelector('.');
-let imgAddress = document.querySelector('.');
+let imgName = document.querySelector('.element__title');
+let imgAddress = document.querySelector('.element__place');
 
 function openPopup() {
   popup.classList.add('popup_opened');
