16e58d79ace7dd5de9c87c1924aead582cb00aef
Мадорная Александра Олеговна <mao@udcs.local>
2020-11-09 15:40:50 +0300

    .

--- scripts/index.js
+++ scripts/index.js
@@ -7,6 +7,7 @@ const btnImgAdd = document.querySelector('.profile__button');
 const btnImgClose = document.querySelector('.popup__close-img');
 const btnLike = document.querySelector('.element__like');
 
+const elementsContainer = document.querySelector('.elements');
 const popupImgContainer = document.querySelector('.popup__container-img');
 const popupContainer = document.querySelector('.popup__container');
 
@@ -20,10 +21,62 @@ const userJob = document.querySelector('.profile__job');
 const imgName = document.querySelector('.element__title');
 const imgAddress = document.querySelector('.element__place');
 
+const initialCards = [
+  {
+    name: 'Архыз',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+  },
+  {
+    name: 'Челябинская область',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+  },
+  {
+    name: 'Иваново',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+  },
+  {
+    name: 'Камчатка',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+  },
+  {
+    name: 'Холмогорский район',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+  },
+  {
+    name: 'Байкал',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+  }
+];
+
+const cardData = {
+  name: inputImgName.value,
+  link: inputImgAddress.value
+};
+
+initialCards.forEach(function () {
+  const cardElement = createCard(cardData);
+  elementsContainer.prepend(cardElement);
+})
+
+function createCard(data) {
+  const elementTemplate = document.querySelector('#elementTemplate').content;
+  // клонируем содержимое тега template
+  const cadrElement = elementTemplate.cloneNode(true);
+  // наполняем содержимым
+  cadrElement.querySelector('.element__place').src = data.link;
+  cadrElement.querySelector('.element__title').textContent = data.name;
+  // отображаем на странице
+  elementsContainer.prepend(cadrElement);
+  return cadrElement;
+}
 function openPopup() {
   popup.classList.add('popup_opened');
   inputName.value = userName.textContent;
   inputJob.value = userJob.textContent;
+}
+
+function openImgPopup(evt) {
+  popupImgAdd.classList.add('popup_opened');
 }
 
 function closePopup() {
@@ -42,42 +95,4 @@ btnClose.addEventListener('click', closePopup);
 btnImgClose.addEventListener('click', closePopup);
 btnEdit.addEventListener('click', openPopup);
 popupContainer.addEventListener("submit", savePopup);
-
-// Скрипт для добавления карточек, удаление карточек:
-// 1. открыть/закрыть попап
-function openImgPopup() {
-  popupImgAdd.classList.add('popup_opened');
-}
 btnImgAdd.addEventListener('click', openImgPopup);
-// 2. добавление новых карточек
-// 3. удалить карточку
-// 4. заменить фоновую картинку:
-
-// 5. плавное появление попапов
-
-// const initialCards = [
-//   {
-//     name: 'Архыз',
-//     link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
-//   },
-//   {
-//     name: 'Челябинская область',
-//     link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
-//   },
-//   {
-//     name: 'Иваново',
-//     link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
-//   },
-//   {
-//     name: 'Камчатка',
-//     link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
-//   },
-//   {
-//     name: 'Холмогорский район',
-//     link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
-//   },
-//   {
-//     name: 'Байкал',
-//     link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
-//   }
-// ];
