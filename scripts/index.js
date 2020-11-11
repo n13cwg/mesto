5543bc8f7f46601b39a614c329ef702dc1a7640b
Мадорная Александра Олеговна <mao@udcs.local>
2020-11-11 10:20:31 +0300

    .

--- scripts/index.js
+++ scripts/index.js
@@ -21,93 +21,91 @@ const userJob = document.querySelector('.profile__job');
 const imgName = document.querySelector('.element__title');
 const imgPlace = document.querySelector('.element__place');
 
-const initialCards = [
-  {
-    name: 'Кафе',
-    link: './images/safar-safarov-MSN8TFhJ0is-unsplash.jpg'
-  },
-  {
-    name: 'TODO list',
-    link: './images/roman-synkevych-vXInUOv1n84-unsplash.jpg'
-  },
-  {
-    name: 'Картинка',
-    link: './images/markus-spiske-MgtHZ4zlC1U-unsplash.jpg'
-  },
-  {
-    name: 'Компьютер',
-    link: './images/goran-ivos-idmvPhF8t4E-unsplash.jpg'
-  },
-  {
-    name: 'Философия',
-    link: './images/ben-kolde-Ajcipx1VDXI-unsplash.jpg'
-  },
-  {
-    name: 'Идеальное рабочее место',
-    link: './images/alexandru-acea--WBYxmW4yuw-unsplash.jpg'
-  }
+const initialCards = [{
+        name: 'Кафе',
+        link: './images/safar-safarov-MSN8TFhJ0is-unsplash.jpg'
+    },
+    {
+        name: 'TODO list',
+        link: './images/roman-synkevych-vXInUOv1n84-unsplash.jpg'
+    },
+    {
+        name: 'Картинка',
+        link: './images/markus-spiske-MgtHZ4zlC1U-unsplash.jpg'
+    },
+    {
+        name: 'Компьютер',
+        link: './images/goran-ivos-idmvPhF8t4E-unsplash.jpg'
+    },
+    {
+        name: 'Философия',
+        link: './images/ben-kolde-Ajcipx1VDXI-unsplash.jpg'
+    },
+    {
+        name: 'Идеальное рабочее место',
+        link: './images/alexandru-acea--WBYxmW4yuw-unsplash.jpg'
+    }
 ];
 // Шесть карточек «из коробки»
 initialCards.forEach(createCard);
 // Добавление карточки
 function createCard(item) {
-  const elementTemplate = document.querySelector('#elementTemplate').content;
-  // клонируем содержимое тега template
-  const cadrElement = elementTemplate.cloneNode(true);
-  // наполняем содержимым
-  cadrElement.querySelector('.element__place').src = item.link;
-  cadrElement.querySelector('.element__title').textContent = item.name;
-  // отображаем на странице
-  elementsContainer.prepend(cadrElement);
-  // Лайк карточки
-  const btnLike = document.querySelector('.element__like');
-  btnLike.addEventListener('click', function () {
-    btnLike.classList.toggle('element__like_black');
-  });
-  // Удаление карточки
-  const btnDelete = document.querySelector('.element__delete-btn');
-  btnDelete.addEventListener('click', function () {
-    const element = btnDelete.closest('.element');
-    element.remove();
-  })
-  return cadrElement;
+    const elementTemplate = document.querySelector('#elementTemplate').content;
+    // клонируем содержимое тега template
+    const cadrElement = elementTemplate.cloneNode(true);
+    // наполняем содержимым
+    cadrElement.querySelector('.element__place').src = item.link;
+    cadrElement.querySelector('.element__title').textContent = item.name;
+    // отображаем на странице
+    elementsContainer.prepend(cadrElement);
+    // Лайк карточки
+    const btnLike = document.querySelector('.element__like');
+    btnLike.addEventListener('click', function() {
+        btnLike.classList.toggle('element__like_black');
+    });
+    // Удаление карточки
+    const btnDelete = document.querySelector('.element__delete-btn');
+    btnDelete.addEventListener('click', function() {
+            const element = btnDelete.closest('.element');
+            element.remove();
+        })
+        // Открытие попапа с картинкой
+
+    return cadrElement;
 }
 // Добавление новой карточки
 function saveImgPopup(evt) {
-  evt.preventDefault();
-  const cardData = {
-    name: inputImgName.value,
-    link: inputImgAddress.value
-  };
-  const cardElement = createCard(cardData);
-  elementsContainer.prepend(cardElement);
-  popupImgContainer.reset(cardData);
-  closePopup();
+    evt.preventDefault();
+    const cardData = {
+        name: inputImgName.value,
+        link: inputImgAddress.value
+    };
+    const cardElement = createCard(cardData);
+    elementsContainer.prepend(cardElement);
+    popupImgContainer.reset(cardData);
+    closePopup();
 }
 // Открытие попапа профиля
 function openPopup() {
-  popup.classList.add('popup_opened');
-  inputName.value = userName.textContent;
-  inputJob.value = userJob.textContent;
+    popup.classList.add('popup_opened');
+    inputName.value = userName.textContent;
+    inputJob.value = userJob.textContent;
 }
 // Открытие попапа для добавления картинки
 function openImgPopup() {
-  popupImgAdd.classList.add('popup_opened');
+    popupImgAdd.classList.add('popup_opened');
 }
-
-// Открытие попапа с картинкой
-
 // Закрытие попапа
 function closePopup() {
-  popup.classList.remove('popup_opened');
-  popupImgAdd.classList.remove('popup_opened');
+    popup.classList.remove('popup_opened');
+    popupImgAdd.classList.remove('popup_opened');
 }
 // Сохранение данных пользователя на странице
 function savePopup(evt) {
-  evt.preventDefault();
-  userName.textContent = inputName.value;
-  userJob.textContent = inputJob.value;
-  closePopup();
+    evt.preventDefault();
+    userName.textContent = inputName.value;
+    userJob.textContent = inputJob.value;
+    closePopup();
 }
 
 btnClose.addEventListener('click', closePopup);
