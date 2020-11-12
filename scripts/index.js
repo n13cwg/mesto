4cdf96c08cfa722070e3c0ab0935bdd6ccf4b453
Мадорная Александра Олеговна <mao@udcs.local>
2020-11-12 16:39:15 +0300

    rename const

--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,5 @@
 const popup = document.querySelector('.popup');
-const popupImgAdd = document.querySelector('.popup__create-card');
+const popupCreateCard = document.querySelector('.popup__create-card');
 
 const btnEdit = document.querySelector('.profile__edit-button');
 const btnClose = document.querySelector('.popup__close');
@@ -8,14 +8,14 @@ const btnImgClose = document.querySelector('.popup__close-create');
 const btnClosePicture = document.querySelector('.popup__close-picture');
 
 const elementsContainer = document.querySelector('.elements');
-const popupImgContainer = document.querySelector('.popup__create-container');
+const popupCreateContainer = document.querySelector('.popup__create-container');
 const popupContainer = document.querySelector('.popup__container');
-const popupImgShow = document.querySelector('.popup__picture');
+const popupPicture = document.querySelector('.popup__picture');
 
 const inputName = document.querySelector('.popup__input_name');
 const inputJob = document.querySelector('.popup__input_job');
-const inputImgName = document.querySelector('.popup__input_name-card');
-const inputImgAddress = document.querySelector('.popup__input_address');
+const inputCardName = document.querySelector('.popup__input_name-card');
+const inputImgLink = document.querySelector('.popup__input_link');
 
 const userName = document.querySelector('.profile__name');
 const userJob = document.querySelector('.profile__job');
@@ -52,12 +52,12 @@ initialCards.forEach(createCard);
 function createCard(item) {
     const elementTemplate = document.querySelector('#elementTemplate').content;
     // клонируем содержимое тега template
-    const cadrElement = elementTemplate.cloneNode(true);
+    const cardElement = elementTemplate.cloneNode(true);
     // наполняем содержимым
-    cadrElement.querySelector('.element__place').src = item.link;
-    cadrElement.querySelector('.element__title').textContent = item.name;
+    cardElement.querySelector('.element__place').src = item.link;
+    cardElement.querySelector('.element__title').textContent = item.name;
     // отображаем на странице
-    elementsContainer.prepend(cadrElement);
+    elementsContainer.prepend(cardElement);
     // Лайк карточки
     const btnLike = document.querySelector('.element__like');
     btnLike.addEventListener('click', function() {
@@ -72,25 +72,24 @@ function createCard(item) {
     // Открытие попапа с картинкой
     const imgPlace = document.querySelector('.element__place');
     imgPlace.addEventListener('click', function () {
-        popupImgShow.classList.add('popup_opened');
+        popupPicture.classList.add('popup_opened');
         const picture = document.querySelector('.popup__image');
-        const pictureCaption = document.querySelector('.popup__image-caption');
+        const pictureCaption = document.querySelector('.popup__image_caption');
         picture.src = item.link;
-        console.log(pictureCaption, item.name);
         pictureCaption.textContent = item.name;
     })
-    return cadrElement;
+    return cardElement;
 }
 // Добавление новой карточки
-function saveImgPopup(evt) {
+function saveCard(evt) {
     evt.preventDefault();
     const cardData = {
-        name: inputImgName.value,
-        link: inputImgAddress.value
+        name: inputCardName.value,
+        link: inputImgLink.value
     };
     const cardElement = createCard(cardData);
     elementsContainer.prepend(cardElement);
-    popupImgContainer.reset(cardData);
+    popupCreateContainer.reset(cardData);
     closePopup();
 }
 // Открытие попапа профиля
@@ -101,13 +100,13 @@ function openPopup() {
 }
 // Открытие попапа для добавления картинки
 function openImgPopup() {
-    popupImgAdd.classList.add('popup_opened');
+    popupCreateCard.classList.add('popup_opened');
 }
 // Закрытие попапа
 function closePopup() {
     popup.classList.remove('popup_opened');
-    popupImgAdd.classList.remove('popup_opened');
-    popupImgShow.classList.remove('popup_opened');
+    popupCreateCard.classList.remove('popup_opened');
+    popupPicture.classList.remove('popup_opened');
 }
 // Сохранение данных пользователя на странице
 function savePopup(evt) {
@@ -123,4 +122,4 @@ btnClosePicture.addEventListener('click', closePopup);
 btnEdit.addEventListener('click', openPopup);
 popupContainer.addEventListener("submit", savePopup);
 btnImgAdd.addEventListener('click', openImgPopup);
-popupImgContainer.addEventListener("submit", saveImgPopup);
+popupCreateContainer.addEventListener("submit", saveCard);
