70f603230a6e7837b81195df9a6e43c51a325941
n13cwg <n13cwg@gmail.com>
2020-11-15 23:59:21 +0300

    123

--- scripts/index.js
+++ scripts/index.js
@@ -20,100 +20,109 @@ const inputImgLink = document.querySelector('.popup__input_link');
 const userName = document.querySelector('.profile__name');
 const userJob = document.querySelector('.profile__job');
 
+const elementTemplate = document.querySelector('#elementTemplate').content;
+const picture = document.querySelector('.popup__image');
+const pictureCaption = document.querySelector('.popup__caption');
+
 const initialCards = [{
-        name: 'Кафе',
-        link: './images/safar-safarov-MSN8TFhJ0is-unsplash.jpg'
-    },
-    {
-        name: 'TODO list',
-        link: './images/roman-synkevych-vXInUOv1n84-unsplash.jpg'
-    },
-    {
-        name: 'Картинка',
-        link: './images/markus-spiske-MgtHZ4zlC1U-unsplash.jpg'
-    },
-    {
-        name: 'Компьютер',
-        link: './images/goran-ivos-idmvPhF8t4E-unsplash.jpg'
-    },
-    {
-        name: 'Философия',
-        link: './images/ben-kolde-Ajcipx1VDXI-unsplash.jpg'
-    },
-    {
-        name: 'Идеальное рабочее место',
-        link: './images/alexandru-acea--WBYxmW4yuw-unsplash.jpg'
-    }
+  name: 'Кафе',
+  link: './images/safar-safarov-MSN8TFhJ0is-unsplash.jpg'
+},
+  {
+    name: 'TODO list',
+    link: './images/roman-synkevych-vXInUOv1n84-unsplash.jpg'
+  },
+  {
+    name: 'Картинка',
+    link: './images/markus-spiske-MgtHZ4zlC1U-unsplash.jpg'
+  },
+  {
+    name: 'Компьютер',
+    link: './images/goran-ivos-idmvPhF8t4E-unsplash.jpg'
+  },
+  {
+    name: 'Философия',
+    link: './images/ben-kolde-Ajcipx1VDXI-unsplash.jpg'
+  },
+  {
+    name: 'Идеальное рабочее место',
+    link: './images/alexandru-acea--WBYxmW4yuw-unsplash.jpg'
+  }
 ];
 // Шесть карточек «из коробки»
 initialCards.forEach(createCard);
 // или initialCards.forEach((item) => createCard(item));
 // Добавление карточки
 function createCard(item) {
-    const elementTemplate = document.querySelector('#elementTemplate').content;
-    // клонируем содержимое тега template
-    const cardElement = elementTemplate.cloneNode(true);
-    // наполняем содержимым
-    cardElement.querySelector('.element__place').src = item.link;
-    cardElement.querySelector('.element__title').textContent = item.name;
-    // отображаем на странице
-    elementsContainer.prepend(cardElement);
-    // Лайк карточки
-    const btnLike = document.querySelector('.element__like');
-    btnLike.addEventListener('click', function() {
-        btnLike.classList.toggle('element__like_black');
-    });
-    // Удаление карточки
-    const btnDelete = document.querySelector('.element__delete-btn');
-    btnDelete.addEventListener('click', function() {
-            const element = btnDelete.closest('.element');
-            element.remove();
-        })
-    // Открытие попапа с картинкой
-    const imgPlace = document.querySelector('.element__place');
-    imgPlace.addEventListener('click', function () {
-        popupPicture.classList.add('popup_opened');
-        const picture = document.querySelector('.popup__image');
-        const pictureCaption = document.querySelector('.popup__caption');
-        picture.src = item.link;
-        pictureCaption.textContent = item.name;
-    })
-    return cardElement;
+  // const elementTemplate = document.querySelector('#elementTemplate').content;
+  // клонируем содержимое тега template
+  const cardElement = elementTemplate.cloneNode(true);
+  // наполняем содержимым
+  cardElement.querySelector('.element__place').src = item.link;
+  cardElement.querySelector('.element__title').textContent = item.name;
+  // отображаем на странице
+  elementsContainer.prepend(cardElement);
+  // Лайк карточки
+  const btnLike = cardElement.querySelector('.element__like');
+  btnLike.addEventListener('click', function () {
+    btnLike.classList.toggle('element__like_black');
+  });
+  // Удаление карточки
+  const btnDelete = cardElement.querySelector('.element__delete-btn');
+  btnDelete.addEventListener('click', function () {
+    const element = btnDelete.closest('.element');
+    element.remove();
+  })
+  // Открытие попапа с картинкой
+  const imgPlace = cardElement.querySelector('.element__place');
+  imgPlace.addEventListener('click', function () {
+    popupPicture.classList.add('popup_opened');
+    // const picture = document.querySelector('.popup__image');
+    // const pictureCaption = document.querySelector('.popup__caption');
+    picture.src = item.link;
+    pictureCaption.textContent = item.name;
+  })
+  return cardElement;
 }
+
 // Добавление новой карточки
 function saveCard(evt) {
-    evt.preventDefault();
-    const cardData = {
-        name: inputCardName.value,
-        link: inputImgLink.value
-    };
-    const cardElement = createCard(cardData);
-    elementsContainer.prepend(cardElement);
-    popupCreateContainer.reset(cardData);
-    closePopup();
+  evt.preventDefault();
+  const cardData = {
+    name: inputCardName.value,
+    link: inputImgLink.value
+  };
+  const cardElement = createCard(cardData);
+  elementsContainer.prepend(cardElement);
+  popupCreateContainer.reset(cardData);
+  closePopup();
 }
+
 // Открытие попапа профиля
 function openPopup() {
-    popup.classList.add('popup_opened');
-    inputName.value = userName.textContent;
-    inputJob.value = userJob.textContent;
+  popup.classList.add('popup_opened');
+  inputName.value = userName.textContent;
+  inputJob.value = userJob.textContent;
 }
+
 // Открытие попапа для добавления картинки
 function openImgPopup() {
-    popupCreateCard.classList.add('popup_opened');
+  popupCreateCard.classList.add('popup_opened');
 }
+
 // Закрытие попапа
 function closePopup() {
-    popup.classList.remove('popup_opened');
-    popupCreateCard.classList.remove('popup_opened');
-    popupPicture.classList.remove('popup_opened');
+  popup.classList.remove('popup_opened');
+  popupCreateCard.classList.remove('popup_opened');
+  popupPicture.classList.remove('popup_opened');
 }
+
 // Сохранение данных пользователя на странице
 function savePopup(evt) {
-    evt.preventDefault();
-    userName.textContent = inputName.value;
-    userJob.textContent = inputJob.value;
-    closePopup();
+  evt.preventDefault();
+  userName.textContent = inputName.value;
+  userJob.textContent = inputJob.value;
+  closePopup();
 }
 
 btnClose.addEventListener('click', closePopup);
